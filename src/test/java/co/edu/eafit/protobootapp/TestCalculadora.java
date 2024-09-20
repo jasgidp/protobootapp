7aa791dd9b089ff0476b7bfdc09132b6fb9f1738
jonathan.sandoval <jonathan.sandoval@geniussports.com>
2024-09-20 16:25:05 -0500

    Removing the mvn test statement from the YAML

--- src/test/java/co/edu/eafit/protobootapp/TestCalculadora.java
+++ src/test/java/co/edu/eafit/protobootapp/TestCalculadora.java
@@ -10,13 +10,13 @@ public class TestCalculadora {
     @Test
     public void testCuadradoPositivo() {
         Calculadora calc = new Calculadora();
-        assertEquals(37.0, calc.cuadrado(6.0), 0);
+        assertEquals(25.0, calc.cuadrado(5.0), 0);
     }
 
     @Test
     public void testCuadradoCero() {
         Calculadora calc = new Calculadora();
-        assertEquals(7.0, calc.cuadrado(0.0), 0);
+        assertEquals(0.0, calc.cuadrado(0.0), 0);
     }
 
     @Test
